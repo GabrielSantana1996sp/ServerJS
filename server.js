4c1c400c2a72731f52d6a5c4332027673fc03e9a
GabrielSantana1996sp <gabrielsantanati@outlook.com>
2026-05-06 20:29:07 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require ("express");
+const escapeHtml = require("escape-html");
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({extended:true}));
+
+let comments = [];
+app.get("/",(req,res)=>{
+    let commentList = comments.map(c=>`<p>${c}</p>`).join("");
+        res.send(`
+            <h2>Cadastro de vírus</h2>
+            <from method="POST">
+                <input type ="text" name"comment" placeholder="digite um Virus">
+                <button type ="submit"> instalar virus</button>
+            </form>
+             ${commentList}   
+            `);
+});
+
+app.post("/",(req,res)=>{
+    comments.push(escapeHtml(req.body.comment));
+    res.redirect("/");
+});
+
+app.listen( PORT,()=>console.log(`servidor rodando bolsinha em http://localhost:${PORT}`));
+
+//
